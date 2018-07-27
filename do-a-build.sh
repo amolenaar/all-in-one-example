58288daa318dc1ab7c571119fa898e0d670da6a1
Arjan Molenaar <gaphor@gmail.com>
2018-07-27 15:24:55 +0200

    Add support for Elixir

--- do-a-build.sh
+++ do-a-build.sh
@@ -2,11 +2,6 @@
 
 # Do a build: build software and dependencies based on simple Git heuristics
 
-function die() {
-  echo "FAIL: $*" >&2
-  exit 1
-}
-
 COMMIT_RANGE=${1:-HEAD}
 
 echo "Do a build for ${COMMIT_RANGE}"
@@ -14,6 +9,8 @@ echo "Do a build for ${COMMIT_RANGE}"
 git diff --name-only ${COMMIT_RANGE} | grep '[^.].*/' | cut -f1 -d/ | sort -u | \
 while read PROJECT
 do
+  echo "Project to build: $PROJECT"
   test -f "${PROJECT}/gradlew" && { cd "${PROJECT}" && ./gradlew build; }
+  test -f "${PROJECT}/mix.exs" && { cd "${PROJECT}" && mix test; }
 done
 
